323c66fa2c7ffa705fcdb23ec760516de28d7144
AOx0 <50227494+AOx0@users.noreply.github.com>
2021-12-01 15:57:56 -0600

    -a & -c Flags

--- main.c
+++ main.c
@@ -11,6 +11,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <libc.h>
 
 #include "Carta.h"
 #include "Mano.h"
@@ -37,7 +38,57 @@ void evaluarMesa(Mesa * mesa) {
     mostrarGanador(mesa);
 }
 
-int main( void ) {
+int main( int argc, char **argv ) {
+
+    if (argc != 1) {
+        if (strcmp(argv[1], "-a") == 0)
+            printf("Los 20 juegos fueron: \n"
+                   "N   Crupier|Jugador            Ganador\n\n"
+                   "1.  0 -> 2 & 0 -> 2         -- Crupier\n"
+                   "2.  0 -> 2 & 0 -> 0         -- Crupier\n"
+                   "3.  2 -> 2 & 0 -> 0         -- Crupier\n"
+                   "4.  2 -> 2 & 0 -> 2         -- Jugador\n"
+                   "5.  6 -> 6 & 3 -> 3         -- Crupier\n"
+                   "6.  2 -> 2 & 0 -> 0         -- Crupier\n"
+                   "7.  2 -> 4 & 0 -> 2         -- Crupier\n"
+                   "8.  2 -> 3 & 3 -> 3         -- Crupier\n"
+                   "9.  5 -> 5 & 6 -> 6         -- Jugador\n"
+                   "10. 2 -> 2 & 0 -> 0         -- Crupier\n"
+                   "11. 0 -> 2 & 0 -> 2         -- Jugador\n"
+                   "12. 2 -> 2 & 2 -> 2         -- Jugador\n"
+                   "13. 3 -> 3 & 0 -> 2 -> 0    -- Crupier\n"
+                   "14. 2 -> 2 & 0 -> 2         -- Crupier\n"
+                   "15. 0 -> 2 & 3 -> 3         -- Jugador\n"
+                   "16. 2 -> 4 & 2 -> 3         -- Crupier\n"
+                   "17. 2 -> 3 & 2 -> 2         -- Crupier\n"
+                   "18. 2 -> 2 & 2 -> 2         -- Crupier\n"
+                   "19. 2 -> 2 & 2 -> 2         -- Jugador\n"
+                   "20. 0 -> 2 & 2 -> 2         -- Crupier\n"
+                   "\n"
+                   "Promedio de mejora del Crupier: 0.7\n"
+                   "Promedio de mejora del Jugador: 0.55\n"
+                   "\n"
+                   "Victorias Crupier: 14\n"
+                   "Victorias Jugador: 6\n"
+                   "\n"
+                   "Es clara la superioridad del Crupier en mejora de jugada.\n"
+                   "Es importante mencionar que el Crupier nada sabe sobre la\n"
+                   "próxima carta que saldrá en el Deck de cartas ni sabe nada\n"
+                   "sobre las cartas del jugador, es simple suerte y \"habilidad\".\n");
+        if (strcmp(argv[1], "-c") == 0)
+            printf(
+                    "Los cambios fueron nulos, el Crupier mostró ser competente\n"
+                    "desde la primera versión de su algoritmo.\n"
+                    "Si tiene una jugada donde las 5 cartas que tiene se usan,\n"
+                    "entonces no realiza una acción y se da por servido.\n"
+                    "\n"
+                    "Si tiene una mala jugada, primero se deshace de las cartas\n"
+                    "más bajas de su mano. Cada que cambia una carta evalúa su\n"
+                    "puntuación y revisa cuál es su carta más baja para elegir.\n"
+            );
+        puts(" ");
+        return 0;
+    }
 
 
     while (1) {
@@ -120,12 +171,14 @@ int main( void ) {
             case 4:
 
                 puts("El Crupier cambiará algunas cartas: \n");
-
+                printf("Puntos antes: %d\n", mesa.manoUno.puntos);
                 {
                     int changedValues[3] = {-1, -1, -1};
                     juegaCrupier(&mesa, 0, 0, mesa.manoUno.puntos, changedValues);
                 }
                 puts("El Crupier ya cambió sus cartas. Es tu turno.\n");
+                evaluaUnaMano(&mesa.manoUno);
+                printf("Puntos después: %d\n", mesa.manoUno.puntos);
 
 
                 char quiereCambio;
